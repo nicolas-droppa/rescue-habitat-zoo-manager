e0bf33441357751a7b5474950a0f2475630479cf
nicolas-droppa <droppa.nicolas@gmail.com>
2025-08-20 21:34:32 +0200

    now pathing works --bug when i click on wall it freezes

--- Assets/Scripts/Entities/Worker/WorkerStateWalkTo.cs
+++ Assets/Scripts/Entities/Worker/WorkerStateWalkTo.cs
@@ -1,16 +1,20 @@
-using UnityEngine;
-
 public class WorkerStateWalkTo : WorkerState
 {
-    public WorkerStateWalkTo(Worker worker) : base(worker) {}
+    public WorkerStateWalkTo(Worker worker) : base(worker) { }
+
+    public override void Enter()
+    {
+        // Nothing special for now
+    }
 
     public override void Update()
     {
+        // Just move along the path each frame
         worker.MoveAlongPath();
+    }
 
-        if (worker.IsAtTarget())
-        {
-            worker.OnArrivedAtJob();
-        }
+    public override void Exit()
+    {
+        // Nothing to clean up
     }
 }
